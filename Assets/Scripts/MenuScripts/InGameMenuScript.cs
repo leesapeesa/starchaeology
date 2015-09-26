31da26bea5ca2158e95c875d5f0fe1ca32d76c40
lbnelson <lbnelson@hmc.edu>
2015-09-25 21:50:47 -0700

    Adding in-game menu script

---
+++ Assets/Scripts/MenuScripts/InGameMenuScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InGameMenuScript : MonoBehaviour {
+
+    public Canvas pauseMenu;
+    public Button pauseButton;
+
+	// Use this for initialization
+	void Start () {
+        pauseMenu = pauseMenu.GetComponent<Canvas>();
+        pauseButton = pauseButton.GetComponent<Button>();
+        pauseMenu.enabled = false;
+	}
+	
+    public void PausePress () {
+        pauseMenu.enabled = true;
+        pauseButton.enabled = false;
+        Time.timeScale = 0;
+
+        // enable buttons in the pause menu here
+    }
+
+    public void ResumePress () {
+        pauseMenu.enabled = false;
+        pauseButton.enabled = true;
+        Time.timeScale = 1;
+    }
+
+    public void ExitGame () {
+        Application.Quit();
+    }
+}
