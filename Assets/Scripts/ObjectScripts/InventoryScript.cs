0a225dcac7a22eeb0263c5eb2dc5f86ad9efa9d0
Lisa Yin <lyin@g.hmc.edu>
2015-10-29 10:45:58 -0700

    you can remove items

--- Assets/Scripts/ObjectScripts/InventoryScript.cs
+++ Assets/Scripts/ObjectScripts/InventoryScript.cs
@@ -7,13 +7,42 @@ using System;
 
 public class InventoryScript : MonoBehaviour {
 
+    // We can't use Pair or Tuple in Unity and we need it for our
+    // inventory to store a copy of collectible and the amount
+    // since each collectible is a unique object.
+    // Potentially make this into its own class.
+    private struct Entry
+    {
+        public Collectible item;
+        public int amount;
+
+        public Entry (Collectible item, int amount) {
+            this.item = item;
+            this.amount = amount;
+        }
+
+        public static Entry operator ++(Entry e) {
+            e.amount++;
+            return e;
+        }
+
+        public static Entry operator --(Entry e) {
+            e.amount--;
+            return e;
+        }
+
+        public bool Empty() {
+            return amount == 0;
+        }
+    }
+
     public static InventoryScript inventory;
 
     public float inX, inY = 0f;
     public int totalSlots = 20;
     public GameObject slot;
 
-    private Dictionary<Collectible, int> inventorySlots;
+    private Dictionary<string, Entry> inventorySlots;
     private GameObject inventoryGUI;
     private GameObject slotPanel;
     private PlayerCharacter2D player;
@@ -30,7 +59,7 @@ public class InventoryScript : MonoBehaviour {
     }
 
     void SetUpInventory() {
-        inventorySlots = new Dictionary<Collectible, int>();
+        inventorySlots = new Dictionary<string, Entry>();
         print ("setting up inventory");
     }
 
@@ -41,45 +70,46 @@ public class InventoryScript : MonoBehaviour {
     // Use this for initialization
     void Start () {
         slotPanel = GameObject.Find ("SlotPanel");
-        if (GameObject.Find ("Player") != null)
-            player = GameObject.Find ("Player").transform.GetComponent<PlayerCharacter2D> ();
         audioSource = GetComponent<AudioSource>();
     }
 
     public void AddItemToInventory(Collectible item) {
         print ("added Item to inventory!");
-        if (!inventorySlots.ContainsKey (item))
-            inventorySlots.Add(item, 1);
+        if (!inventorySlots.ContainsKey (item.type))
+            inventorySlots.Add(item.type, new Entry(item, 1));
         else
-            ++inventorySlots [item];
+            inventorySlots [item.type]++;
     }
 
     public void RemoveItemFromInventory(Collectible item) {
         Assert.IsNotNull (item);
-        if (!inventorySlots.ContainsKey (item)) {
+        if (!inventorySlots.ContainsKey (item.type)) {
             print ("Item missing");
             return;
         }
         // Update GameObject Counter;
+        player = GameObject.Find ("Player").transform.GetComponent<PlayerCharacter2D> ();
         item.OnUse(player);
-        --inventorySlots [item];
-        if (inventorySlots [item] == 0) {
-            inventorySlots.Remove(item);
+        --inventorySlots [item.type];
+        if (inventorySlots [item.type].Empty()) {
+            inventorySlots.Remove(item.type);
         }
+        DrawInventory();
         audioSource.Play();
     }
 
     public void DrawInventory() {
         print ("Drawing Inventory");
         RemoveAllOldSlots ();
-        foreach (KeyValuePair<Collectible, int> pair in inventorySlots) {
+        foreach (KeyValuePair<string, Entry> pair in inventorySlots) {
             print ("pair" + pair.Key.ToString());
-            Collectible item = pair.Key;
+            Entry entry = pair.Value;
+            Collectible collectible = entry.item;
             // We need to make the slot.
             GameObject go = Instantiate (slot, new Vector3 (0, 0, 0), Quaternion.identity) as GameObject;
-            go.transform.GetComponent<SlotScript>().item = item;
-            go.transform.FindChild ("Item").gameObject.GetComponent<Image> ().sprite = item.itemIcon;
-            go.transform.FindChild("Counter").gameObject.GetComponent<Text>().text = pair.Value.ToString();
+            go.transform.GetComponent<SlotScript>().item = collectible;
+            go.transform.FindChild ("Item").gameObject.GetComponent<Image> ().sprite = collectible.itemIcon;
+            go.transform.FindChild("Counter").gameObject.GetComponent<Text>().text = entry.amount.ToString();
             go.transform.SetParent (slotPanel.transform);
             go.transform.localScale = new Vector3 (1, 1, 1);
         }
