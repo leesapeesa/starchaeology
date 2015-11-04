381685da8ec2c324f1aa173e745edd5b659df2bf
Jonathan Chang <jonathan.chang13@gmail.com>
2015-11-03 19:46:52 -0800

    Prep for merge

--- Assets/Scripts/LevelScripts/Difficulty.cs
+++ Assets/Scripts/LevelScripts/Difficulty.cs
@@ -25,11 +25,19 @@ public class Difficulty {
     private readonly Dictionary<DifficultySetting, int> startValues;
     private readonly Dictionary<DifficultySetting, int> endValues;
     //Constant coefficients relating the internal numeric difficulty encoding to various parameters
-    private const float HEIGHT_COEFF = 0.5f;
+    private const float HEIGHT_COEFF = 1f / 4f;
+    private const float HEIGHT_OFFSET = 5;
     private const float ENEMIES_COEFF = 1f / 12f;
     private const float ENEMIES_OFFSET = 2;
     private const float SIDE_LENGTH_COEFF = 5;
     private const float POISON_COEFF = 1f / 400f;
+    private const float FREQ_COEFF = 1f / 10f;
+    private const float FREQ_OFFSET = 4;
+    private const float OCTAVES_COEFF = 1f / 50f;
+    private const float OCTAVES_OFFSET = 1;
+    private const float LACUNARITY_COEFF = 1f / 125f;
+    private const float LACUNARITY_OFFSET = 1;
+    private const float GAIN_COEFF = 1f / 375f;
 
     public Difficulty(DifficultySetting startSetting, int levels)
     {
@@ -57,13 +65,23 @@ public class Difficulty {
     /// </summary>
     public void UpdateDifficultyValue(int curLevel)
     {
+        //difficulty increases linearly with increasing level. The slope is determined by the start and end difficulty
+        //values and the number of levels.
         difficulty = (int)(startValues[setting] + curLevel * (float)(endValues[setting] - startValues[setting]) / numLevels);
     }
 
+    /// <summary>
+    /// Update the values of the PersistentTerrainSettings parameters based on current level
+    /// </summary>
     public void UpdateTerrainParameters(PersistentTerrainSettings settings)
     {
-        settings.height = HEIGHT_COEFF * difficulty;
+        settings.height = HEIGHT_OFFSET + HEIGHT_COEFF * difficulty;
         settings.sideLength = SIDE_LENGTH_COEFF * difficulty;
+        settings.frequency = FREQ_OFFSET + FREQ_COEFF * difficulty;
+        settings.octaves = (int)(OCTAVES_OFFSET + OCTAVES_COEFF * difficulty);
+        settings.lacunarity = LACUNARITY_OFFSET + LACUNARITY_COEFF * difficulty;
+        settings.gain = GAIN_COEFF * difficulty;
+
         if (difficulty > 30 && difficulty < 70) {
             settings.textureType = TerrainTextureType.Grassy;
         } else if (difficulty >= 0 && difficulty < 30) {
@@ -73,6 +91,9 @@ public class Difficulty {
         }
     }
 
+    /// <summary>
+    /// Update the values of the PersistentLevelSettings parameters based on current level
+    /// </summary>
     public void UpdateLevelParameters(PersistentLevelSettings settings)
     {
         settings.numEnemies = (int)(ENEMIES_OFFSET + ENEMIES_COEFF * difficulty);
